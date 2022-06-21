a83ee0bd9ed55fe5537cb25dd962ea7711210e13
Marwan <achbari.marouane@gmail.com>
2022-06-21 20:32:35 +0100

    ItemFactory class created

---
+++ src/main/java/com/gildedrose/domain/ItemFactory.java
@@ -0,0 +1,23 @@
+package com.gildedrose.domain;
+
+import com.gildedrose.interfaces.ItemNames;
+import com.gildedrose.interfaces.UpdateQuality;
+
+public class ItemFactory {
+    public UpdateQuality getItem(String itemType, String name, int sellIn, int quality) {
+        switch (itemType) {
+            case ItemNames.BACKSTAGE:
+                return new Backstage(name, sellIn, quality);
+            case ItemNames.AGED_BRIE:
+                return new AgedBrie(name, sellIn, quality);
+            case ItemNames.CONJURED:
+                return new Conjured(name, sellIn, quality);
+            case ItemNames.SULFURAS:
+                return new Sulfuras(name, sellIn, quality);
+            case ItemNames.NORMAL_ITEM:
+                return new NormalItem(name, sellIn, quality);
+            default:
+                return null;
+        }
+    }
+}
