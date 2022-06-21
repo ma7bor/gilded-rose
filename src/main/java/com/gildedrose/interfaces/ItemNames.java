f97fcaca4c42cdbf2c92e88bf251f2ff3d93d540
Marwan <achbari.marouane@gmail.com>
2022-06-21 20:29:36 +0100

    itemNames class created

---
+++ src/main/java/com/gildedrose/interfaces/ItemNames.java
@@ -0,0 +1,9 @@
+package com.gildedrose.interfaces;
+
+public interface ItemNames {
+   String BACKSTAGE = "BackStage";
+   String AGED_BRIE = "Aged Brie";
+   String CONJURED = "Conjured";
+   String SULFURAS = "Sulfuras";
+   String NORMAL_ITEM = "NoramlItem";
+}
